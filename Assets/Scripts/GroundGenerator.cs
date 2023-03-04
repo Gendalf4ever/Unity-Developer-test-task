b3787784378cc96cda923016df5f82cabe92897d
master <chelovek438@gmail.com>
2023-03-04 03:54:42 +0300

    added auto deleting old grounds

--- Assets/Scripts/GroundGenerator.cs
+++ Assets/Scripts/GroundGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GroundGenerator : MonoBehaviour
 {
     public GameObject[] groundPrefabs;
+    private List<GameObject> activeGrounds = new List<GameObject>();
     private float spawnPosition = 0;
     private float groundLength = 100;
     [SerializeField] private Transform player;
@@ -17,15 +18,27 @@ public class GroundGenerator : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (player.position.x - 60 > spawnPosition - (startGroundsCount * groundLength))
+        {
+            SpawnGround(Random.Range(0, groundPrefabs.Length));
+            DeleteGround();
+        }
+
     }
     private void SpawnGround(int groundIndex)
     {
         var pos = transform.position.x;
-        Instantiate(groundPrefabs[groundIndex], transform.right * spawnPosition, transform.rotation);
-         spawnPosition += groundLength;
+       GameObject nextGround = Instantiate(groundPrefabs[groundIndex], transform.right * spawnPosition, transform.rotation);
+        activeGrounds.Add(nextGround);
+        spawnPosition += groundLength;
+    }
+
+    private void DeleteGround()
+    {
+        Destroy(activeGrounds[0]);
+        activeGrounds.RemoveAt(0);
     }
 }
